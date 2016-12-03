9a7ac70f2bb7aa07360526608c76fc708ae5ecd3
brickgale <brianmonsales@gmail.com>
2016-12-04 07:27:49 +0800

    update header

--- src/js/components/Logo.js
+++ src/js/components/Logo.js
@@ -5,12 +5,18 @@ export default class Logo extends React.Component {
 	render() {
 
 		const logo_url = "http://placehold.it/40x60&amp;text=C";
+		const logo_url_2 = "http://placehold.it/220x60&amp;text=CarService Corp";
 
 		return (
 			<div>
 				<a href="" class="logo-icon simple">
 					<img src={logo_url} />
 				</a>
+				<h1 class="logo">
+					<a href="">
+						<img src={logo_url_2} />
+					</a>
+				</h1>
 			</div>
 		);
 	}
