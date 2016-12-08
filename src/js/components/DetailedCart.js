c3b10543905bd54bcb1c0cf8516e54badbe9c4c8
brickgale <brianmonsales@gmail.com>
2016-12-09 06:33:43 +0800

    update detailcart.js

--- src/js/components/DetailedCart.js
+++ src/js/components/DetailedCart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Links } from "react-router";
-import Request from 'superagent';
+import Request from 'request';
 
 export default class DetailedCart extends React.Component {
 
@@ -10,13 +10,17 @@ export default class DetailedCart extends React.Component {
 	}
 
 	componentWillMount() {
-		var getCartUrl = "http://localhost:8181/cart";
-		Request.get(getCartUrl).then((response) => {
-			console.log(response);
-			this.setState({
-				cart: response.body
-			});
-		});
+		var CookieJar = unirest.jar();
+		CookieJar.add('cart_id=1', '/'); // Cookie string, pathname / url
+		//intialize products
+		const endpoint_url = "http://localhost:8181/";
+		const product_ids = ["22565423428", "22565423394"];
+
+		for(var product_id of product_ids) {
+			Request.post(endpoint_url+'cart/'+product_id).cookie('cart_id=1');
+		}
+
+		Request.get(endpoint_url+'cart').cookie('cart_id=1');
 	}
 
 	render() {
